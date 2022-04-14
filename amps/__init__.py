b65744ad68b341a366343a9ba6dc5393ab081bec
Henry Robbins <hwr26@cornell.edu>
2022-04-14 12:53:03 -0400

    Added module from deterministic-sim repo.

---
+++ amps/__init__.py
@@ -0,0 +1,14 @@
+"""
+amps
+====
+
+amps offers a maintainable parameter management system for applications
+requiring both deterministic and non-stochastic parameters. It is compatible
+with hierarchical parameter structures specified in either YAML or JSON format.
+Furthermore, parameters can be defined as the (simple) function of other
+parameters within YAML/JSON files reducing coupling and increasing readability.
+"""
+
+__author__ = "Cornell Covid Modeling Team"
+
+from scenario import ScenarioFamily
